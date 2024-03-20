90cccfcc7d62d37b0ff07b388f6297ac5c9c55d6
Angel <angeldariaux@gmail.com>
2024-03-19 20:40:47 -0600

    created movies endpoint with a file that contains the diccionary

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse  # html response in the endpoints
+from movies_list_dic import movies
 
 app = FastAPI()
 app.title = "Mi applicacion con FastAPI"
 app.version = "0.0.1"
 
+movies_list = movies
+
+
 @app.get('/', tags=['Home'])
 def message():
-  return "Hola mundo"
+  return HTMLResponse('<h1>Hola Mundo!</h1>')
+
+@app.get('/movies', tags=['movies'])
+def get_movies():
+  return movies_list
